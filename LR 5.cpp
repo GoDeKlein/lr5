9281ce82e2be8681f1e3754a90486610c36e15a0
GoDeKlein <tkachartyom51@gmail.com>
2020-10-26 23:03:31 +0300

    add LR 5.cpp

---
+++ LR 5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double L;
+    cout << "Введіть довжину в сантиметрах" << endl;
+    cin >> L;
+    L = (int)L / 100;
+    cout << "Довжина в метрах складає: " << L << endl;
+    system("pause");
+
+}
